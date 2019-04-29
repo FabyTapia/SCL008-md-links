2776f918ff99e946a4bfa862954eaafeb82c0acd
FabyTapia <ftapiamorales@gmail.com>
2019-04-29 18:33:44 -0400

    se hace 2do it de test

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -4,8 +4,12 @@ const mdLinks = require('../md-links');
 describe('mdLinks', () => {
 
   it('deberia retornar 2 links que se encuentran en md-linkspage.md ', ()=> {
-      expect(mdLinks.readAFile('md-linkspage.md')).resolves.toEqual(['https://help.github.com/en/articles/creating-an-issue',
+      expect(mdLinks.readAFile('md-linkspage.md','utf-8')).resolves.toEqual(['https://help.github.com/en/articles/creating-an-issue',
       'https://www.npmjs.com/package/package' ])
   });
+    it('deberia retornar error si no es links en md-links.js',()=>{
+      expect(mdLinks.readAFile('md-linkspage.md','utf-8')).resolves.toEqual(['error.message']);
+    });
 
 });
+
--- test/md-linkspage.md
+++ test/md-linkspage.md
@@ -1,3 +1,4 @@
 https://help.github.com/en/articles/creating-an-issue,
 https://www.npmjs.com/package/package;
 
+https://ww.npmjs.com/package/package;
